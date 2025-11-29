55432d3ea72369a6cb94eeaaa26808de0b656dba
eirvav <eirik.vavik@hotmail.no>
2025-11-29 05:38:00 +0100

    disabled scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${spaceGrotesk.variable} antialiased`}
+        className={`${spaceGrotesk.variable} antialiased overflow-hidden h-screen w-screen`}
         style={{ fontFamily: 'var(--font-space-grotesk)' }}
       >
         {/* <Navbar /> */}
